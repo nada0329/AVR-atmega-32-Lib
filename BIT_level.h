2fa531968cd2f623873d32dfadde644bf717c4d2
unknown <nada66811@gmail.com>
2022-01-22 13:48:31 +0200

    create basic headers for the system

---
+++ BIT_level.h
@@ -0,0 +1,14 @@
+#ifndef BIT_LEVEL_H_
+#define BIT_LEVEL_H_
+
+#define assignbit(var,num,val) var|=(val<<num)
+#define clrbit(var,num) var&=~(1<<num)
+#define togbit(var,num) var^=(1<<num)
+#define setbit(var,num) var|=(1<<num)
+#define sethighnibble(var) var|=0xf0
+#define setlownibble(var) var|=0x0f
+#define clrlownibble(var) var&=0xf0
+#define clrhighnibble(var) var&=0x0f
+#define getbit(var,num) (var>>num)& 0x01
+
+#endif /* BIT_LEVEL_H_ */
