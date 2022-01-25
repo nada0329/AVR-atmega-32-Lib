79ec8645b9f493201a0db83909e9899861c8a736
unknown <nada66811@gmail.com>
2022-01-25 22:11:13 +0200

    keypad private header file

---
+++ HAL/Keypad/Keypad_private.h
@@ -0,0 +1,14 @@
+#ifndef KEYPAD_PRIVATE_H_
+#define KEYPAD_PRIVATE_H_
+
+
+#define eq   10
+#define modl 11
+#define mul  12
+#define div  13
+#define add  14
+#define sub  15
+
+u8 DecideKey(u8 num);
+
+#endif /* KEYPAD_PRIVATE_H_ */
