0b9b65cbe3f4cc626c47a938fe62019e2e38a87c
unknown <nada66811@gmail.com>
2022-01-25 22:10:17 +0200

    keypad pins config file

---
+++ HAL/Keypad/Keypad_conf.h
@@ -0,0 +1,16 @@
+#ifndef KEYPAD_CONF_H_
+#define KEYPAD_CONF_H_
+
+#define input_port APORT
+#define output_port APORT
+#define R0 Pin0
+#define R1 Pin1
+#define R2 Pin2
+#define R3 Pin3
+#define C0 Pin4
+#define C1 Pin5
+#define C2 Pin6
+#define C3 Pin7
+
+
+#endif /* KEYPAD_CONF_H_ */
