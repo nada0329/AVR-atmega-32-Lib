bec7b0eac5a83a58d268422a9ad3a8b18714adc5
unknown <nada66811@gmail.com>
2022-01-25 22:10:45 +0200

    keypad user interface file

---
+++ HAL/Keypad/Keypad_int.h
@@ -0,0 +1,21 @@
+#ifndef KEYPAD_INT_H_
+#define KEYPAD_INT_H_
+
+#define not_pressed 0xff
+
+void KEYPAD_voidInit(void);
+u8 KEYPAD_voidGetKey(void);
+
+
+/*
+	Buttons values
+
+	0	1	2	3
+	4	5	6	7
+	8	9	=	%
+	*	/	+	-
+	
+*/
+
+
+#endif /* KEYPAD_INT_H_ */
