dc03253a1063e65ec2c9926fdcd7d807a5a14b4b
unknown <nada66811@gmail.com>
2022-02-03 07:20:36 +0200

    external int headers

---
+++ MCAL/EXTI_conf.h
@@ -0,0 +1,16 @@
+#ifndef _EXTI_CONF_H_
+#define _EXTI_CONF_H_
+
+/*********conf options********/
+/*Low Level Sense Control*/
+/*Falling Edge Sense Control*/
+/*Rising Edge Sense Control*/
+/*On Change Sense Control*/
+
+#define EXTI0_SENSE_CONTROL EXTI_ON_CHANGE_SC
+#define EXTI1_SENSE_CONTROL EXTI_ON_CHANGE_SC
+#define EXTI2_SENSE_CONTROL EXTI_FALLING_EDGE_SC
+
+
+
+#endif /*_EXTI_CONF_H_*/
